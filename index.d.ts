b38ec234259d180a428c95f083277b35a93372e6
Link <link19970507@gmail.com>
2020-01-03 15:39:27 +0800

    fix: supports compiler function to ide

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
-export declare class Render {
+import { RenderType } from "./lib/render"
+
+export declare class Render implements RenderType{
   constructor(option?: object)
   set(html: string): void
   render(template?: string): string
+  compiler(template: string): any[]
 }
